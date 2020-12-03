e34904d5c51e423e109294d52bde3ed4b9d26545
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2020-12-03 15:21:59 +0330

    fix: DEFAULT_FONT_PATH

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 ```bash
 npm i google-font-family-downloader
 ```
+
+## Usage
+
+```js
+import { join } from 'path';
+import { downloadGoogleFontFamily } from 'google-font-family-downloader';
+
+const FONTS_DIRECTORY = join(process.cwd(), `fonts`);
+
+// some where else
+const fonts = await downloadGoogleFontFamily('Roboto', FONTS_DIRECTORY);
+```
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
+import { join } from 'path';
 import axios from 'axios';
 import { Open } from 'unzipper';
 
-export async function downloadGoogleFontFamily(family: string, path: string = process.cwd()) {
+const DEFAULT_FONT_PATH = join(process.cwd(), `fonts`);
+
+/**
+ * 
+ * @param family Google font family
+ * @param path where to save fonts, by default `./fonts/`
+ */
+export async function downloadGoogleFontFamily(family: string, path: string = DEFAULT_FONT_PATH) {
   const { data } = await axios(`https://fonts.google.com/download`, {
     responseType: 'arraybuffer',
     params: { family },
   });
   const { files, extract } = await Open.buffer(data);
-  extract({ path, concurrency: 5 });
+  extract({ path: join(path, family), concurrency: 5 });
   return files;
 }
